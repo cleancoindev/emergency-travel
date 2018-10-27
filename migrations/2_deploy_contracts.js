da82c2c81d83010c8684eb637be004058d050d2a
Dennison Bertram <dennison@dennisonbertram.com>
2018-10-27 10:41:07 +0200

    Removed Excess Contract Deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,26 +1,36 @@
-var SimpleStorage = artifacts.require("SimpleStorage");
-var TutorialToken = artifacts.require("TutorialToken");
-var ComplexStorage = artifacts.require("ComplexStorage");
-const WTIndex = artifacts.require('WTIndex');
+// var SimpleStorage = artifacts.require("SimpleStorage");
+// var TutorialToken = artifacts.require("TutorialToken");
+// var ComplexStorage = artifacts.require("ComplexStorage");
+const WTIndex = artifacts.require("WTIndex");
 
 module.exports = function(deployer, network, accounts) {
-  console.log('Network:', network);
-  console.log('Accounts:', accounts);
+  console.log("Network:", network);
+  console.log("Accounts:", accounts);
 
-  deployer.deploy(SimpleStorage);
-  deployer.deploy(TutorialToken);
-  deployer.deploy(ComplexStorage);
+  // deployer.deploy(SimpleStorage);
+  // deployer.deploy(TutorialToken);
+  // deployer.deploy(ComplexStorage);
 
   const lifTokenAddress =
-  network == 'mainnent'
-    ? '0xeb9951021698b42e4399f9cbb6267aa35f82d59d'
-    : '0x5FDFBa355A30FB00ee12965cf3a1c24CA8DF77FB';
+    network == "mainnent"
+      ? "0xeb9951021698b42e4399f9cbb6267aa35f82d59d"
+      : "0x5FDFBa355A30FB00ee12965cf3a1c24CA8DF77FB";
 
-deployer.deploy(WTIndex, 100, "Air France: Syria Rescue").then(function (wtIndexContract) {
-  console.log('WTIndex address:', wtIndexContract.address);
-  wtIndexContract.setLifToken(lifTokenAddress).then(function (tx) {
-    console.log('LifToken set on tx:', tx.tx);
-  });
-});
+  deployer
+    .deploy(WTIndex, 100, "Air France: Syria Rescue")
+    .then(function(wtIndexContract) {
+      console.log("WTIndex address:", wtIndexContract.address);
+      wtIndexContract.setLifToken(lifTokenAddress).then(function(tx) {
+        console.log("LifToken set on tx:", tx.tx);
+      });
+      wtIndexContract.registerHotel("Familly of Three", 3).then(function(tx) {
+        console.log("Familly of three registered", tx.tx);
+      });
+      wtIndexContract.registerHotel("Familly of Four", 4).then(function(tx) {
+        console.log("Familly of four registered", tx.tx);
+      });
+      wtIndexContract.registerHotel("Familly of 6", 6).then(function(tx) {
+        console.log("Familly of six registered", tx.tx);
+      });
+    });
 };
-
